0e97a2e0acfaf9fe8f7118304ac4b72f0b68ddd9
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-02-28 10:53:15 +0530

    Create output.tf

---
+++ multistage-env-setup/output.tf
@@ -0,0 +1,10 @@
+utput "ec2_public_ip" {
+  value = [ for instance in aws_instance.my_ec2 : instance.public_ip]
+}
+output "ec2_public_dns" {
+  value = [ for instance in aws_instance.my_ec2 : instance.public_dns]
+}
+
+output "ec2_private_ip" {
+  value = [ for instance in aws_instance.my_ec2 : instance.private_ip]
+}
