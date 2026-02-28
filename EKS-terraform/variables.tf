59731091bb085fef1a889a720f01920279ae0721
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-02-28 23:38:02 +0530

    Create variables.tf

---
+++ EKS-terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "knox-eks-cluster"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "intra_subnets" {
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
